3aa69575d1043da941fe462c517834c2034be51d
tsalman80 <tsalman80@gmail.com>
2022-03-10 17:59:52 -0800

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,13 @@ import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ListItemComponent } from './shared/components/list-item/list-item.component';
 import { NgModule } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    CommonModule,
-    DragDropModule
-  ],
-  entryComponents: [
-    ListItemComponent,
-    CdkDragDropNestedListsExample
-  ],
-  declarations: [
-    ListItemComponent,
-    CdkDragDropNestedListsExample
-  ],
-  bootstrap: [
-    CdkDragDropNestedListsExample
-  ],
-  providers: []
+  imports: [BrowserModule, CommonModule, DragDropModule, CardModule],
+  entryComponents: [ListItemComponent, CdkDragDropNestedListsExample],
+  declarations: [ListItemComponent, CdkDragDropNestedListsExample],
+  bootstrap: [CdkDragDropNestedListsExample],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {}
